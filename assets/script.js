ed97a1480c34c263609dfea2b783a9904b21726a
Kkadaija25 <kadaijadortch@icloud.com>
2022-11-08 18:42:15 -0600

    added to assets

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var userInput = window.prompt("How long do you want your password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+ var password = prompt("Input a password greater than 128 characters")
+  if (password.length < = 8){
+    window.alert("Password length must be 128 characters greater than 8 characters")
+    return
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
